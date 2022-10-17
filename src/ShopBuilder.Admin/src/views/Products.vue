e09920753a20468f4d48b4e1d8c6ce25305beee5
Jakub Kuniszewski <you@example.com>
2022-10-17 13:51:01 +0200

    Create blank view for Products

---
+++ src/ShopBuilder.Admin/src/views/Products.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="products">
+    <header>
+      <p class="products__breadcrumb">Catalog / Products</p>
+      <BaseHeader>Products</BaseHeader>
+    </header>
+
+  </div>
+</template>
+
+<style lang="scss" src="./Products.scss" scoped/>
+
+<script lang="ts" setup>
+import BaseHeader from '../components/BaseHeader.vue';
+
+</script>
