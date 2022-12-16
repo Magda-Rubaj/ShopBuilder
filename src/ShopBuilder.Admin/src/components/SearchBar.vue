0e9b21d82194e2686d3be270469578ac5b9b735c
Jakub Kuniszewski <you@example.com>
2022-12-16 21:02:26 +0100

    Create Searchbar and Table components

---
+++ src/ShopBuilder.Admin/src/components/SearchBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="searchbar">
+    <div class="searchbar__frame">
+      <span class="searchbar__icon" v-html="lupeIcon"/>
+      <input :value="modelValue" @input="handleUpdate($event)" class="searchbar__input"/>
+    </div>
+    <button class="searchbar__btn">Search</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import lupeIcon from '/src/assets/lupe.svg?raw';
+defineProps({
+  modelValue: {
+    type: String,
+    required: false,
+    default: ''
+  }
+});
+const emit = defineEmits(['update:modelValue']);
+const handleUpdate = (e: any) => emit('update:modelValue', e.target.value);
+</script>
+
+<style lang="scss" scoped src="./SearchBar.scss"/>
---
+++ src/ShopBuilder.Admin/src/components/Table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="table">
+    <div class="table__controls">
+      <span v-html="filterIcon"/>
+      <SearchBar/>
+    </div>
+    <div class="table__content">
+      <table class="table__table">
+        <thead class="table__head">
+          <th>ID</th>
+          <th>Image</th>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Price (Tax excl.)</th>
+          <th>Price (Tax incl.)</th>
+          <th>Quantity</th>
+          <th>Description</th>
+        </thead>
+        <tbody class="table__body">
+          <tr>
+            <td>1</td>
+            <td>img.jpg</td>
+            <td>S1</td>
+            <td>Biodegradable</td>
+            <td>3.99 $</td>
+            <td>5 $</td>
+            <td>999</td>
+            <td>Better send it back</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="table__pagination">Insert premade pagination component</div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import SearchBar from './SearchBar.vue';
+import filterIcon from '/src/assets/filter.svg?raw';
+
+</script>
+
+<style lang="scss" scoped src="./Table.scss"/>
